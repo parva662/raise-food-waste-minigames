22da5d06339afdff8dc578e9dea080c4bb8eba16
parva662 <parvaneh550@gmail.com>
2026-07-21 15:38:03 +0200

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/raise-food-waste-minigames/' : '/',
   plugins: [react()],
   test: {
     environment: 'node',
   },
-});
+}));
